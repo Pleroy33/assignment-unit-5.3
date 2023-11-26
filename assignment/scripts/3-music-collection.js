017e4997d5e424ed0d9984f5964dd3cdcdbbe11d
Paul LeRoy <pleroy33@gmail.com>
2023-11-26 16:17:37 -0600

    wrote a function for taking album parameters and returning as an object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,11 +1,48 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 myCollection = [];
+function addToCollection(collection, title, artist, yearPublished) {
+  console.log('addToCollection is running');
+  console.log('collection is : ', collection);
+  console.log('album is : ', title);
+  console.log('artist is : ', artist);
+  console.log('yearPublished is :', yearPublished);
+
+  newObject = {};
+  newObject.title = title;
+  newObject.artist = artist;
+  newObject.yearPublished = yearPublished;
+  collection.push(newObject);
+  console.log(newObject);
+  return newObject;
+}
+addToCollection(myCollection,'Reprise','Moby','2021');
+addToCollection(myCollection, 'Lets Go', 'Rancid','1994');
+addToCollection(myCollection,'IV', 'badbadnotgood','2016');
+addToCollection(myCollection,'Tape Deck Heart', 'Frank Turner & the Sleeping Souls', '2013');
+addToCollection(myCollection, 'MICHAEL','Killer Mike','2023');
+addToCollection(myCollection, 'Streetcore', 'Joe Strummer', '2003');
+  
+  
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//add 6 albums to myCollection array
 
 
 
